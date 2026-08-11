cdd43cde19821f966c9583cbd0070db7a7655c35
Eilaf Aljundi <elafjundi@gmail.com>
2026-08-11 14:20:47 -0400

    render server

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -5,7 +5,8 @@ const isLocalhost = window.location.hostname === 'localhost'
 const api = axios.create({
     baseURL: isLocalhost
         ? 'http://localhost:5000'
-        : 'https://sekure-password-server.vercel.app',
+        // : 'https://sekure-password-server.vercel.app',
+        : 'https://sekurepasswordserver.onrender.com/',
     withCredentials: true
 })
 
